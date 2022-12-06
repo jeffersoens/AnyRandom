470a4f29c32d14fd7ba82c883fdda93f844e82d1
Hunyakamolodoy <dmitriy_seleznev_01@mail.ru>
2022-12-07 01:41:13 +0700

    first build without alerts

---
+++ AnyRandom/ViewControllers/LotteryViewController.swift
@@ -0,0 +1,82 @@
+//
+//  LotteryViewController.swift
+//  randomLetter
+//
+//  Created by Дмитрий Селезнев on 02.12.2022.
+//
+
+import UIKit
+
+class LotteryViewController: UIViewController {
+    
+// MARK: - IB Outlets
+    @IBOutlet var countTextField: UITextField!
+    @IBOutlet var minimumTextField: UITextField!
+    @IBOutlet var maximumTextField: UITextField!
+    
+    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var copyButton: UIButton!
+    
+//MARK: - Private Properties
+    private var minimumNumber = 0
+    private var maximumNumber = 0
+    private var countOfGroup = 0
+    
+    private let pasteboard = UIPasteboard.general
+    
+// MARK: - Life Cycles Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        answerLabel.text = ""
+        titleLabel.text = ""
+        copyButton.isHidden = true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    @IBAction func copyFromAnswerLabel() {
+        //copyText(for: answerLabel) Раскомментить
+    }
+    
+    @IBAction func getNumbers() {
+        titleLabel.text = "Итог:"
+        copyButton.isHidden = false
+        //answerLabel.text = getArray().formatted()
+    }
+    
+//MARK: - Private Methods
+    /*
+    private func getArray() -> [String] {
+        minimumNumber = Int(minimumTextField.text ?? "") ?? 0
+        maximumNumber = Int(maximumTextField.text ?? "") ?? 0
+        countOfGroup = Int(countTextField.text ?? "") ?? 1
+        
+        if minimumNumber < maximumNumber {
+            var numbers = [Int]()
+            for _ in 1...countOfGroup {
+                numbers.append(Int.random(in: minimumNumber...maximumNumber))
+            }
+        } else {
+            /* Раскомментить
+            showAlert(
+                title: "Упс... Ошибочка",
+                message: "Похоже Вы вышли за рамки границ",
+                actionTitle: "Ок"
+            )
+            minimumTextField.text = ""
+            maximumTextField.text = ""
+            countTextField.text = ""
+            copyButton.isHidden = true
+            */
+        }
+        
+        //let array = numbers.map { $0.description }
+        //return "[array]"
+    }
+    */
+}
---
+++ AnyRandom/ViewControllers/PredictViewController.swift
@@ -0,0 +1,40 @@
+//
+//  YesNoController.swift
+//  randomLetter
+//
+//  Created by Дмитрий Селезнев on 01.12.2022.
+//
+
+import UIKit
+
+class PredictViewController: UIViewController {
+    
+// MARK: - IB Outlets
+    @IBOutlet var quesionTextField: UITextField!
+    
+    @IBOutlet var emojiLabel: UILabel!
+    @IBOutlet var describeLabel: UILabel!
+    
+// MARK: - Life Cycles Methods
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    @IBAction func letKnowAnswer() {
+        let countOfAnswers = Int.random(in: 0...2)
+        switch countOfAnswers {
+        case 0:
+            showAnswer(emoji: Emoji.yes.rawValue, describe: Emoji.yes.describe)
+        case 1:
+            showAnswer(emoji: Emoji.no.rawValue, describe: Emoji.no.describe)
+        default:
+            showAnswer(emoji: Emoji.dontKnow.rawValue, describe: Emoji.dontKnow.describe)
+        }
+    }
+    
+    private func showAnswer(emoji: Character, describe: String) {
+        emojiLabel.text = "\(emoji)"
+        describeLabel.text = describe
+    }
+}
